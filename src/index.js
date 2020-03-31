f98739792740cb4199741bd6931060e286cb29ec
Hugo Azevedo <hugo.soares@outlook.com>
2020-03-31 13:15:02 -0300

    feat: add helmet middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import morgan from 'morgan'
 import routes from './routes'
 
 const app = express()
 
+app.use(morgan('dev'))
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
-app.use(morgan('dev'))
 app.disable('x-powered-by')
 
 app.use('/api', routes)
